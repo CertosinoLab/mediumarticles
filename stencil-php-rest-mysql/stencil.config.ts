7873a6a2db393c7517faac46e213ce6c6a41f332
CertosinoLab <certosinolab@gmail.com>
2021-08-23 18:25:13 +0200

    commit 1

---
+++ stencil-php-rest-mysql/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'stencil-php-rest-mysql',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
