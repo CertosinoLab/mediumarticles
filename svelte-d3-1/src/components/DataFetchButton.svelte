b8edd57b68252483eb7f1e1f469c35981785447a
CertosinoLab <certosinolab@gmail.com>
2021-09-02 15:12:25 +0200

    Commit 5

--- svelte-d3-1/src/components/DataFetchButton.svelte
+++ svelte-d3-1/src/components/DataFetchButton.svelte
@@ -1,10 +1,6 @@
 <script>
     import {sharedData} from "../store";
 
-    function getRandomInt(max) {
-        console.log(Math.floor(Math.random() * max))
-    }
-
     function fetchData() {
         sharedData.set([Math.floor(Math.random() * 600),
             Math.floor(Math.random() * 600),
