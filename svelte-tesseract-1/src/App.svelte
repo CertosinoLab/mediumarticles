5d644fd82136837dec05d82cfcda9d1a5ddc83b6
CertosinoLab <certosinolab@gmail.com>
2021-09-07 18:07:27 +0200

    commit 2

--- svelte-tesseract-1/src/App.svelte
+++ svelte-tesseract-1/src/App.svelte
@@ -1,10 +1,23 @@
 <script>
+	import Tesseract from 'tesseract.js';
+
+	function logTesseract() {
+		Tesseract.recognize(
+				'https://tesseract.projectnaptha.com/img/eng_bw.png',
+				'eng',
+				{ logger: m => console.log(m) }
+		).then(({ data: { text } }) => {
+			console.log(text);
+		})
+	}
+
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={logTesseract}>Log Tesseract</button>
 </main>
 
 <style>
