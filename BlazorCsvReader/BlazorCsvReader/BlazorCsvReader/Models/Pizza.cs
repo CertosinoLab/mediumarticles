e9ca438f5f1944ae2c2ae0da33349af4420f4098
certosinolab <certosinolab@gmail.com>
2022-01-09 07:17:11 +0100

    Blazor Csv Reader

---
+++ BlazorCsvReader/BlazorCsvReader/BlazorCsvReader/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorCsvReader.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
+
+        public Pizza(int id, string name, float price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
+    }
+}
