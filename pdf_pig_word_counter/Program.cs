133d046cbae64facb0a28c4c77f9c3cae01e0362
certosinolab <certosinolab@gmail.com>
2022-02-12 23:03:49 +0100

    1

---
+++ pdf_pig_word_counter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace pdf_pig_word_counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToFind = "pancake";
+            int numberOfOccurrences = 0;
+
+            using (PdfDocument document = PdfDocument.Open(@"YOUR PATH\pdf\test.pdf"))
+            {
+                foreach (Page page in document.GetPages())
+                {
+                    string pageText = page.Text;
+
+                    foreach (Word word in page.GetWords())
+                    {
+                        if (word.Text.ToLower().Contains(wordToFind.ToLower()))
+                            numberOfOccurrences++;
+                    }
+                }
+                Console.WriteLine("Total Occurrences: " + numberOfOccurrences);
+            }
+        }
+
+
+    }
+}
